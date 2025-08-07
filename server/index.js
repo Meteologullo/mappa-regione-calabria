44d4546d14a5a7b239d4ee10d41970c11298ab76
Meteologullo <angelologullo21@gmail.com>
2025-08-07 18:36:25 +0200

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path    = require('path');
+const app     = express();
+
+app.use(express.static(path.join(__dirname, '..')));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server locale su http://localhost:${PORT}`));
